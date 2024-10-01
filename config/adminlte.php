4b1245fe6c46cb1ec1943d6554863e6f6a5f4fdf
Paty Varela <patrela@hotmail.com>
2024-10-01 15:36:33 -0500

    Personalize Style

--- config/adminlte.php
+++ config/adminlte.php
@@ -65,8 +65,9 @@ return [
     |
     */
 
-    'logo' => '<b>Tech</b>Company',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo' => '<b>Tech</b>MASTERUNION',
+    /*'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png', */
+    'logo_img' => 'img/icono.ico',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
--- resources/css/Plantillas/layouts/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer margen-interno">
-    <nav class="pie">
-        <a href="https://edutin.com" target="_blank">Desarrollado por &copy; Edutin Academy</a>
-    </nav>
-</footer>
